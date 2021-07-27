c8e35740613a0db4b03ed4d4860bbd2ade239c41
opusvar <sprojdev@gmail.com>
2021-07-27 14:00:53 -0400

    Added greetPlanet function

--- main.go
+++ main.go
@@ -2,11 +2,40 @@ package main
 
 import f "fmt"
 
-func fuelGuage(fuel int) int{
+func fuelGuage(fuel int) int {
 	f.Println("Remaining Fuel:", fuel)
     return fuel
 }
 
+// adapt to format input string to title case
+
+func calculateFuel(planet string) int {
+	var fuelNeeded int
+    switch planet {
+	    case "Venus":
+			fuelNeeded = 300000
+		case "Mercury":
+			fuelNeeded = 500000
+		case "Mars":
+			fuelNeeded = 700000
+		default:
+			fuelNeeded = 0
+			f.Print("Planet not found")
+
+	}
+	f.Println("Calculating fuel needs for trip to", planet)
+	f.Println("Fuel needed for trip", fuelNeeded)
+    return fuelNeeded
+}
+
+func greetPlanet(planet string) {
+	f.Println("Attention Passengers and Crew.")
+	f.Println("We've arrived at", planet)
+
+}
+
 func main(){
-    fuelGuage(30)
+	greetPlanet("Genesis")
+
+
 }
